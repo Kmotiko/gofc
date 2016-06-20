d8673e0cad3af14d4340e1ec0bc00c85b5c986f8
Kmotiko <mottii.gen@gmail.com>
2016-06-20 21:32:28 +0900

    add app_manager

---
+++ lib/gofc/app_manager.go
@@ -0,0 +1,28 @@
+package gofc
+
+type IFMap map[string]interface{}
+
+type AppManager struct{
+	applications []interface{}
+}
+
+var appManager *AppManager = newAppManager()
+
+func newAppManager() *AppManager{
+	manager := new(AppManager)
+	manager.applications = make([]interface{}, 0)
+	return manager
+}
+
+func GetAppManager() *AppManager{
+	return appManager
+}
+
+func (manager *AppManager)RegistApplication(app interface{}) {
+	manager.applications = append(manager.applications, app)
+}
+
+func (manager *AppManager)GetApplications() []interface{}{
+	return appManager.applications
+}
+
