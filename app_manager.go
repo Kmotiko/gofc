85a1771c86add841ee52f7f377adaa12be29bff2
Kmotiko <mottii.gen@gmail.com>
2017-02-18 17:51:43 +0900

    fix GetApplications function

--- app_manager.go
+++ app_manager.go
@@ -2,27 +2,26 @@ package gofc
 
 type IFMap map[string]interface{}
 
-type AppManager struct{
+type AppManager struct {
 	applications []interface{}
 }
 
 var appManager *AppManager = newAppManager()
 
-func newAppManager() *AppManager{
+func newAppManager() *AppManager {
 	manager := new(AppManager)
 	manager.applications = make([]interface{}, 0)
 	return manager
 }
 
-func GetAppManager() *AppManager{
+func GetAppManager() *AppManager {
 	return appManager
 }
 
-func (manager *AppManager)RegistApplication(app interface{}) {
+func (manager *AppManager) RegistApplication(app interface{}) {
 	manager.applications = append(manager.applications, app)
 }
 
-func (manager *AppManager)GetApplications() []interface{}{
-	return appManager.applications
+func (manager *AppManager) GetApplications() []interface{} {
+	return manager.applications
 }
-
